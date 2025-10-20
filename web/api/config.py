2bcde5784cf5932da70cdf78b2d5c99d166e46fb
Simon H <github@pointbeing.net>
2025-10-20 05:33:40 +0100

    Begin restructuring

--- web/api/config.py
+++
@@ -1,8 +0,0 @@
-
-SOLVER_FILE = 'parser/solver.pl'
-SOLVER_PRED = 'solution(A, B, C, D), halt'
-COUNTER_PRED = 'solution_count(A, B, C, D, Count), writeln(Count), halt.'
-
-EXAMPLE_DIR = 'web/api/static/examples/'
-UPLOAD_DIR = 'web/api/static/uploads/'
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg'} # TODO: explore others
